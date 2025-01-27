b9e7dd427207a9f8291285e493fb387dcdb1fd3c
Mohamed Raafat <mraafat179@gmail.com>
2025-01-27 15:14:42 +0200

    Created Pay Now button functionality

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -120,7 +120,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
             }
             if (applicationUser.CompanyId.GetValueOrDefault() == 0)
             {
-                var domain = "http://localhost:5152/";
+                var domain = "https://localhost:5152/";
                 var options = new Stripe.Checkout.SessionCreateOptions
                 {
                     SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
